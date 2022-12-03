efacb9181320b758d9455cb92194c2f1f161bdde
Dennis Brink <dennis.mjknd@gmail.com>
2022-12-04 00:30:50 +0100

    MainActivity

--- app/src/main/java/com/dennis_brink/android/ferdithefly/MainActivity.java
+++ app/src/main/java/com/dennis_brink/android/ferdithefly/MainActivity.java
@@ -1,14 +1,28 @@
 package com.dennis_brink.android.ferdithefly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageViewFerdi, imageViewGerm, imageViewMine, imageViewWhirlwind, imageViewCoin, imageViewVolume;
+    AppCompatButton btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        imageViewFerdi = findViewById(R.id.imageViewFerdi);
+        imageViewGerm = findViewById(R.id.imageViewGerm);
+        imageViewMine = findViewById(R.id.imageViewMine);
+        imageViewWhirlwind = findViewById(R.id.imageViewWhirlwind);
+        imageViewCoin = findViewById(R.id.imageViewCoin);
+        imageViewVolume = findViewById(R.id.imageViewVolume);
+
     }
 }

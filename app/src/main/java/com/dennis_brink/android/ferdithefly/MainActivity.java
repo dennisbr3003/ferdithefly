5c49a562a47ed99b58aee97d59a13c8f5e6b0c38
Dennis Brink <dennis.mjknd@gmail.com>
2022-12-04 14:51:37 +0100

    Animation

--- app/src/main/java/com/dennis_brink/android/ferdithefly/MainActivity.java
+++ app/src/main/java/com/dennis_brink/android/ferdithefly/MainActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageViewFerdi, imageViewGerm, imageViewMine, imageViewWhirlwind, imageViewCoin, imageViewVolume;
     AppCompatButton btnStart;
+
+    private Animation animation;
+
+    HashMap<String, ImageView> characters = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +34,27 @@ public class MainActivity extends AppCompatActivity {
         imageViewCoin = findViewById(R.id.imageViewCoin);
         imageViewVolume = findViewById(R.id.imageViewVolume);
 
+        loadCharacters();
+        setCharacterAnimation();
+
     }
+
+    private void loadCharacters(){
+        characters.put("ferdi", imageViewFerdi);
+        characters.put("germ", imageViewGerm);
+        characters.put("mine", imageViewMine);
+        characters.put("wirlwind", imageViewWhirlwind);
+        characters.put("coin", imageViewCoin);
+    }
+
+    private void setCharacterAnimation(){
+
+        animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.scale_animation);
+
+        for (String key : characters.keySet()) {
+            characters.get(key).setAnimation(animation);
+        }
+
+    }
+
 }

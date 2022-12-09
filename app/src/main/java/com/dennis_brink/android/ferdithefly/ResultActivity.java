72f39d0bf37ca1fa80671b2ff09e2864c81aac97
Dennis Brink <dennis.mjknd@gmail.com>
2022-12-09 01:01:19 +0100

    score and highscore

--- app/src/main/java/com/dennis_brink/android/ferdithefly/ResultActivity.java
+++ app/src/main/java/com/dennis_brink/android/ferdithefly/ResultActivity.java
@@ -3,6 +3,8 @@ package com.dennis_brink.android.ferdithefly;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +13,10 @@ public class ResultActivity extends AppCompatActivity {
 
     TextView textViewResultInfo, textViewResultScore, textViewResultHighScore;
     AppCompatButton btnAgain;
+
+    int score;
+    int highscore;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,33 @@ public class ResultActivity extends AppCompatActivity {
 
         btnAgain = findViewById(R.id.btnResultPlayAgain);
 
-        btnAgain.setOnClickListener(view -> {
+        score = getIntent().getIntExtra("score", 0);
+        textViewResultScore.setText("Your Score: " + score);
 
+        sharedPreferences = this.getSharedPreferences("ferdi", MODE_PRIVATE); // this is the name of the collection of preferences
+        highscore = sharedPreferences.getInt("highscore", 0);
+
+        if(score >= 200){ // 200+ is win
+            if(score >= highscore) {
+                textViewResultInfo.setText("You Win with a new HighScore!");
+                textViewResultHighScore.setText("High Score: " + score);
+                sharedPreferences.edit().putInt("highscore", score).apply();
+            }
+        } else {
+            if (score >= highscore){ // < 200 you died
+                textViewResultInfo.setText("You died; but with a new HighScore!");
+                textViewResultHighScore.setText("High Score: " + score);
+                sharedPreferences.edit().putInt("highscore", score).apply();
+            } else {
+                textViewResultInfo.setText("You are not very good a this. Maybe you should consider a game change");
+                textViewResultHighScore.setText("High Score: " + highscore);
+            }
+        }
+
+        btnAgain.setOnClickListener(view -> {
+            Intent i = new Intent(ResultActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
         });
 
     }
